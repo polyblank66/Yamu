cfe7291aff8eeb0d5b9550ef7b917c490f21ec24
Keijiro Takahashi <keijiro@gmail.com>
2025-07-02 11:07:59 +0900

    Tidy up TestScript

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,7 +5,6 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
